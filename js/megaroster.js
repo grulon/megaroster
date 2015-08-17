bbce564f3239eeb09469752e798e89235f9d53af
Gavin Rulon <gavin.rulon@icloud.com>
2015-08-17 11:53:19 -0400

    Use a different constructor pattern.

--- js/megaroster.js
+++ js/megaroster.js
@@ -1,13 +1,9 @@
-var Megatask = {
-  author: "Davey",
-  hisDeal: "Who the heck knows?",
-  newStudentForm: $('#new_student_form'),  //will return jquery obj  # = css for ID, . = css for class.
-  submitHandler: function(ev) {
-    alert('What!?');
-  },
-  start: function() {
-    this.newStudentForm.submit(this.submitHandler);
+var Megaroster = function() {
+  this.init = function() {
+    alert('What!!');
   }
 };
 
-Megatask.start();
+
+var roster = new Megaroster();
+roster.init();
