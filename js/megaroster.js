6fdc13f8a5b9c5a6d1d24105a4e202826bb053da
Gavin Rulon <gavin.rulon@icloud.com>
2015-08-17 16:31:57 -0400

    Add names to roster and push to array.

--- js/megaroster.js
+++ js/megaroster.js
@@ -1,7 +1,29 @@
 var Megaroster = function() {
+
   this.init = function() {
-    alert('What!!');
-  }
+    var self = this;
+    this.students = [];
+    // console.log(this);
+
+    $('#new_student_form').on('submit', function(ev){
+      // console.log("Students: " + self.students.length);
+      ev.preventDefault();
+      var student_name = $(this.student_name).val();
+
+      //if student is typed in, push student to array
+      if(student_name.trim() !== "") {
+        self.students.push(student_name);
+        console.log("Students: " + self.students.length);
+        // console.log(this);
+
+        //add student to list
+        $('#students').append('<li class="list-group-item">' + student_name + '</li>');
+
+        //clear field
+        $(this.student_name).val('');
+      }
+    });
+  };
 };
 
 
